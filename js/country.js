96ea229c56c6fd31cfa6e27fd579a9ce75fb3daf
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2021-08-27 21:26:11 +0600

    search value dynamic

--- js/country.js
+++ js/country.js
@@ -13,11 +13,27 @@ const showCountry = countrys => {
         div.classList.add('single-country');
         div.innerHTML = `
         <h3>Name: ${country.name}</h3>
-        <h3>Population: ${country.population}</h3>
-        <h3>Capital: ${country.capital}</h3>
-        <h3>Region: ${country.region}</h3>
-        <img src="${country.flag}"></img>
+        <button onclick="countryName('${country.name}')"> details </button>
        `
         document.getElementById('countrys').appendChild(div);
     }
+};
+
+const countryName = (countryName) => {
+    const url = `https://restcountries.eu/rest/v2/name/${countryName}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showCountryDetail(data[0]))
+};
+
+const showCountryDetail = (countryInfo) => {
+    const section = document.getElementById('countryDetails');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <h3>Population: ${countryInfo.population}</h3>
+    <h3>Capital: ${countryInfo.capital}</h3>
+    <h3>Region: ${countryInfo.region}</h3>
+    <img src="${countryInfo.flag}"></img>
+    `
+    section.appendChild(div);
 }
---
+++ js/searchFood.js
@@ -0,0 +1,7 @@
+const search = () => {
+    const inputFild = document.getElementById('search-fild');
+    const inputValue = inputFild.value;
+    console.log(inputValue);
+
+    inputFild.value = '';
+}
